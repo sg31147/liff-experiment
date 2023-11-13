081214a54faab83125895c88a427f8dd4343851d
[ Tanitphon Paniwan ] <lw91945@linecorp.com>
2023-11-13 15:33:43 +0700

    update code to proxy from frontend

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const axios = require('axios')
 const express = require('express')
+const cors = require('cors')
 
 require('dotenv').config()
 
 const app = express()
+app.use(cors())
 const port = 8888
 
 const LINE_API_URL = 'https://api.line.me/v2/bot/message/push'
@@ -20,13 +22,13 @@ const headers = {
   'Authorization': `Bearer ${LINE_ACCESS_TOKEN}`
 }
 
-const sendMessage = async (userUid) => {
+const sendMessage = async (userUid, message) => {
   const body = {
     'to': userUid,
     'messages': [
       {
         'type': 'text',
-        'text': 'Hello Message Mike'
+        'text': message
       }
     ]
   }
@@ -35,10 +37,10 @@ const sendMessage = async (userUid) => {
 }
 
 app.post('/send-message', async (req, res) => {
-  const { userUid } = req.body
+  const { userUid, message } = req.body
 
   try {
-    const response = await sendMessage(userUid)
+    const response = await sendMessage(userUid, message)
     console.log('=== LINE log', response.data)
     res.json({
       message: 'Message OK'
@@ -65,7 +67,7 @@ app.post('/webhook', async (req, res) => {
     const lineUserID = events[0].source.userId
     console.log(events[0])
 
-    const response = await sendMessage(lineUserID)
+    const response = await sendMessage(lineUserID, 'Hello from webhook')
     console.log('=== LINE log', response.data)
   } catch (error) {
     console.log('error', error)
